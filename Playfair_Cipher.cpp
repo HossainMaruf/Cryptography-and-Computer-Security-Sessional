022a601fc1a230ea8ceb825f558f3d8044eae611
HossainMaruf <maruficepustian@gmail.com>
2022-01-05 17:31:15 +0600

    Encryption Process Working

--- Playfair_Cipher.cpp
+++ Playfair_Cipher.cpp
@@ -10,15 +10,17 @@ class Playfair_Cipher {
 	int key_length, message_length, cipher_length;
 	bool key_char_exist[26] = {false}, message_char_exist[26] = {false};
 	std::vector<std::pair<char,char>> vec;
+	std::map<char, std::pair<int, int>> row_column;
 	public:
 		Playfair_Cipher(std::string, std::string, std::string);
 		void make_table();
 		void show_table();
 		void make_pair_of_chars();
 		void show_pair_of_chars();
-		std::pair<int, std::pair<int, int>> row_check();
-		std::pair<int, std::pair<int, int>> column_check();
-		std::pair<int, std::pair<int, int>> diagonal_check();
+		std::pair<std::pair<int, int>, std::pair<int, int>> getRowColumn(char, char);
+		std::pair<int, std::pair<int, int>> row_check(char, char);
+		std::pair<int, std::pair<int, int>> column_check(char, char);
+		std::pair<int, std::pair<int, int>> diagonal_check(char, char);
 		std::string Encryption();
 		std::string Decryption();
 };
@@ -39,17 +41,20 @@ void Playfair_Cipher::make_table() {
 	int i = 0, j = 0;
 	std::string keyword = this->key;
 	for(char ch : keyword) {
+		ch = toupper(ch);
 		// store the character by checking the previous occurance
-		if(!isspace(ch) && (!this->key_char_exist[toupper(ch) % 65])) {
+		if(!isspace(ch) && (!this->key_char_exist[ch % 65])) {
 			// char not exist so make it exist
-			this->key_char_exist[toupper(ch) % 65] = true;
+			this->key_char_exist[ch % 65] = true;
 			// store the char in uppercase
-			this->table[i][j] = toupper(ch);
+			this->table[i][j] = ch;
+			// keep track the location of this char
+			this->row_column.insert({ch, {i, j}}); 
 			// Letter I and J will be placed in same location
 			// char I state will be placed on Index-8
 			// char J state will be placed on Index-9
-			if(toupper(ch) == 'I') this->key_char_exist[9] = true;
-			if(toupper(ch) == 'J') this->key_char_exist[8] = true;
+			if(ch == 'I') this->key_char_exist[9] = true;
+			if(ch == 'J') this->key_char_exist[8] = true;
 			if(j == COLUMN-1) j=0, i++; 
 			else j++;
 		}
@@ -59,6 +64,8 @@ void Playfair_Cipher::make_table() {
 		if(!key_char_exist[c % 65]) {
 			this->key_char_exist[c % 65] = true;
 			this->table[i][j] = c;
+			// same logic for here to map
+			this->row_column.insert({c, {i, j}}); 
 			// same logic for here
 			if(c == 'I') this->key_char_exist[9] = true;
 			if(c == 'J') this->key_char_exist[8] = true;
@@ -103,58 +110,59 @@ void Playfair_Cipher::show_pair_of_chars() {
 	}
 }
 
-std::pair<int, std::pair<int, int>> Playfair_Cipher::row_check() {
-	char ch1 = 'F', ch2 = 'D';
-	int row = -1, col1 = -1, col2 = -1;
-	bool ch1_occurance = false, ch2_occurance = false;
+std::pair<std::pair<int, int>, std::pair<int, int>> Playfair_Cipher::getRowColumn(char ch1, char ch2) {
+	// code for filler letter
+	if(ch2 == 'x') ch2 = toupper(ch2);
+	// find ch1 row and column
+	auto itr1 = this->row_column.find(ch1);
+	int ch1_row = itr1->second.first, ch1_col = itr1->second.second;
 
-	for(int i = 0; i < ROW; i++) {
-		row = i, col1 = -1, col2 = -1;
-		for(int j = 0; j < COLUMN; j++) {
-			if(this->table[i][j] == ch1) {
-				ch1_occurance = true;
-				col1 = j;
-			}
-			if(this->table[i][j] == ch2) {
-				ch2_occurance = true;
-				col2 = j;
-			} 
-			if(ch1_occurance && ch2_occurance) return {row, {col1, col2}};
-		}
-		ch1_occurance = ch2_occurance = false;
-    }
-    return {-1, {-1, -1}};
-}	
+	// find ch2 row and column
+	auto itr2 = this->row_column.find(ch2);
+	int ch2_row = itr2->second.first, ch2_col = itr2->second.second;
 
-std::pair<int, std::pair<int, int>> Playfair_Cipher::column_check() {
-	char ch1 = 'D', ch2 = 'N';
-	int col = -1, row1 = -1, row2 = -1;
-	bool ch1_occurance = false, ch2_occurance = false;
-
-	for(int i = 0; i < COLUMN;  i++) {
-		col = i, row1 = -1, row2 = -1;
-		for(int j = 0; j < ROW; j++) {
-			if(this->table[j][i] == ch1) {
-				ch1_occurance = true;
-				row1 = j;
-			}
-			if(this->table[j][i] == ch2) {
-				ch2_occurance = true;
-				row2 = j;
-			}
-			if(ch1_occurance && ch2_occurance) return {col, {row1, row2}};
-		}
-		ch1_occurance = ch2_occurance = false;
-	}
-	return {-1, {-1, -1}};
-}
-
-std::pair<int, std::pair<int, int>> Playfair_Cipher::diagonal_check() {
-
+	return {{ch1_row, ch1_col}, {ch2_row, ch2_col}};
 }
 
 std::string Playfair_Cipher::Encryption() {
+	// coordinate holding variable for pair of chars
+	std::pair<std::pair<int, int>, std::pair<int, int>> coordinate;
+	// coordinates variable for holding separate location
+	int ch1_row, ch1_col, ch2_row, ch2_col;
+	// iterate through the pair of message
+	for(int i = 0; i < vec.size(); i++) {
+		// get the coordinate of each char
+		coordinate  = getRowColumn(vec[i].first, vec[i].second);
+		ch1_row = coordinate.first.first;
+		ch1_col = coordinate.first.second;
+		ch2_row = coordinate.second.first;
+		ch2_col = coordinate.second.second;
+		if(ch1_row == ch2_row) {
+			 // chars are in same row 
+			// std::cout << "Same Row" << std::endl;
+			int ch1_next_col = (ch1_col == COLUMN - 1) ? 0 : (ch1_col + 1);
+			int ch2_next_col = (ch2_col == COLUMN - 1) ? 0 : (ch2_col + 1);
 
+			this->encrypted_text.push_back(this->table[ch1_row][ch1_next_col]);
+			this->encrypted_text.push_back(this->table[ch1_row][ch2_next_col]);
+
+		} else if(ch1_col == ch2_col) {
+			// chars are in same column
+			// std::cout << "Same col" << std::endl;
+			int ch1_next_row = (ch1_row == ROW - 1) ? 0 : (ch1_row + 1);
+			int ch2_next_row = (ch2_row == ROW - 1) ? 0 : (ch2_row + 1);
+
+			this->encrypted_text.push_back(this->table[ch1_next_row][ch1_col]);
+			this->encrypted_text.push_back(this->table[ch2_next_row][ch1_col]);
+
+		} else {
+			// chars are in diagonal
+			// std::cout << "Diagonal" << std::endl;
+			this->encrypted_text.push_back(this->table[ch1_row][ch2_col]);
+			this->encrypted_text.push_back(this->table[ch2_row][ch1_col]);
+		}
+	}
+	std::cout << this->encrypted_text << std::endl;
 }
 
 std::string Playfair_Cipher::Decryption() {
@@ -164,7 +172,7 @@ std::string Playfair_Cipher::Decryption() {
 int main(int argc, char const *argv[]) {
 
 	std::string key = "Black Hole";
-	std::string message = "balloon";
+	std::string message = "Dfi  dfd";
 	std::string cipher_text = "";
 
 	Playfair_Cipher *playfair_cipher = new Playfair_Cipher(key, message, cipher_text);
